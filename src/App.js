1708fe382924e460a3204c250241dc7588f2ae20
estacioart24 <estacioart24@gmail.com>
2022-11-14 18:32:39 -0800

    create pages and route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
-
+import React from "react";
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import ForgotPass from "./pages/ForgotPass";
 
 function App() {
   return (
     <div>
-     <h1 >ArtSky</h1>
+     <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/forgot-pass' element={<ForgotPass/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+
+      </Routes>
+     </Router>
     </div>
   );
 }
